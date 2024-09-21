4f387c383550df37e91b77467f9f2ce374092cc4
Sergoshakh <sergo.shakh@gmail.com>
2024-09-21 21:15:10 +0300

    Add files via upload

---
+++ var.py
@@ -0,0 +1,7 @@
+cnt_tasks = 12
+cnt_time = 1.5
+course_name = 'Python'
+time_task = cnt_time/cnt_tasks
+print('Курс:',course_name,', всего задач:',cnt_tasks,', затрачено часов:',cnt_time,'среднее время выполнения',time_task,'часа.')
+# c f проще
+print(f'Курс: {course_name}, всего задач: {cnt_tasks}, затрачено часов: {cnt_time}, среднее время выполнения {time_task} часа.')
