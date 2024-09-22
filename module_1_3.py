f73a2b9ee61c9955413dcd589a4c00072360b7ba
Sergoshakh <sergo.shakh@gmail.com>
2024-09-22 06:12:34 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Sergo'
+print('Имя:',name)
+age = 58
+print('Возраст:',age)
+age = 58+42
+print('Новый возраст:',age)
+is_student = True
+print('Является студентом:',is_student)
+print('End of lesson')
